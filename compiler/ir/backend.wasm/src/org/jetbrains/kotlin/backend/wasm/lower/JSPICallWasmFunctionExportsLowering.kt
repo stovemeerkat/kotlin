65aae55e557bdeb42251af841fdb689f9d7c2338
stovemeerkat <ga27hed@mytum.de>
2023-11-11 14:43:14 +0100

    Improve exception handling

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/JSPICallWasmFunctionExportsLowering.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/JSPICallWasmFunctionExportsLowering.kt
@@ -6,17 +6,19 @@
 package org.jetbrains.kotlin.backend.wasm.lower
 
 import org.jetbrains.kotlin.backend.common.DeclarationTransformer
+import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
+import org.jetbrains.kotlin.backend.common.lower.irCatch
 import org.jetbrains.kotlin.backend.wasm.WasmBackendContext
-import org.jetbrains.kotlin.ir.builders.irCall
-import org.jetbrains.kotlin.ir.builders.irCallConstructor
-import org.jetbrains.kotlin.ir.builders.irGet
+import org.jetbrains.kotlin.ir.builders.*
+import org.jetbrains.kotlin.ir.builders.declarations.buildVariable
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.expressions.IrBody
-import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.impl.IrExpressionBodyImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrTypeOperatorCallImpl
 import org.jetbrains.kotlin.ir.types.classOrFail
 import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.types.typeWith
@@ -24,6 +26,21 @@ import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.name.Name
 
 class JSPICallWasmFunctionExportsLowering(val context: WasmBackendContext) : DeclarationTransformer {
+    private val anyType = context.wasmSymbols.any.defaultType
+    private val structRefType = context.wasmSymbols.wasmStructRefType
+    private val coroutineConstructor = context.wasmSymbols.jspiCoroutineClass.owner.primaryConstructor
+        ?: throw RuntimeException("Class kotlin.wasm.internal.JSPICoroutine does not have a primary constructor")
+    private val resultClass = context.irBuiltIns.findClass(Name.identifier("Result"))
+        ?: throw RuntimeException("Could not find Result class")
+    private val resultCompanion = resultClass.owner.companionObject()
+        ?: throw RuntimeException("Could not find companion object for Result class")
+    private val resultSuccess = resultCompanion.getSimpleFunction("success")
+        ?: throw RuntimeException("Could not find function Result.Companion.success")
+    private val resultFailure = resultCompanion.getSimpleFunction("failure")
+        ?: throw RuntimeException("Could not find function Result.Companion.success")
+    private val resultConstructor = resultClass.owner.primaryConstructor
+        ?: throw RuntimeException("Result class has no primary constructor")
+
     override fun transformFlat(declaration: IrDeclaration): List<IrDeclaration>? {
         if (declaration !is IrSimpleFunction) {
             return null
@@ -52,25 +69,10 @@ class JSPICallWasmFunctionExportsLowering(val context: WasmBackendContext) : Dec
 
     private fun generateFunctionBody(arity: Int, function: IrSimpleFunction): IrBody {
         val builder = context.createIrBuilder(function.symbol, function.startOffset, function.endOffset)
-        val anyType = context.wasmSymbols.any.defaultType
-        val structRefType = context.wasmSymbols.wasmStructRefType
         val functionParameter = function.valueParameters[0]
         val completionParameter = function.valueParameters[function.valueParameters.size - 2]
         val coroutineParameter = function.valueParameters[function.valueParameters.size - 1]
-        val functionAnyCast = builder.irCall(context.wasmSymbols.refCastNull, type = anyType).apply {
-            putTypeArgument(0, anyType)
-            putValueArgument(0, builder.irGet(structRefType, functionParameter.symbol))
-        }
-        val functionInterfaceCast = IrTypeOperatorCallImpl(
-            function.startOffset,
-            function.endOffset,
-            functionParameter.type,
-            IrTypeOperator.CAST,
-            functionParameter.type,
-            functionAnyCast,
-        )
-        val coroutineConstructor = context.wasmSymbols.jspiCoroutineClass.owner.primaryConstructor
-            ?: throw RuntimeException("Class kotlin.wasm.internal.JSPICoroutine does not have a primary constructor")
+        val functionAnyCast = buildRefCastToAny(builder, builder.irGet(structRefType, functionParameter.symbol))
         val coroutineConstructorCall = builder.irCallConstructor(
             coroutineConstructor.symbol,
             listOf(function.typeParameters[0].defaultType),
@@ -80,7 +82,7 @@ class JSPICallWasmFunctionExportsLowering(val context: WasmBackendContext) : Dec
         val invokeSymbol = functionParameter.type.classOrFail.getSimpleFunction("invoke")
             ?: throw RuntimeException("${function.name} called with function argument that does not have an 'invoke' method")
         val invokeCall = builder.irCall(invokeSymbol).apply {
-            dispatchReceiver = functionInterfaceCast
+            dispatchReceiver = functionAnyCast
             for (i in 0..<arity) {
                 putValueArgument(i, builder.irCall(context.wasmSymbols.refCastNull, type = anyType).apply {
                     putTypeArgument(0, anyType)
@@ -89,43 +91,42 @@ class JSPICallWasmFunctionExportsLowering(val context: WasmBackendContext) : Dec
             }
             putValueArgument(arity, coroutineConstructorCall)
         }
-        val completionAnyCast = builder.irCall(context.wasmSymbols.refCastNull, type = anyType).apply {
-            putTypeArgument(0, anyType)
-            putValueArgument(0, builder.irGet(structRefType, completionParameter.symbol))
-        }
-        val completionInterfaceCast = IrTypeOperatorCallImpl(
-            function.startOffset,
-            function.endOffset,
-            completionParameter.type,
-            IrTypeOperator.CAST,
-            completionParameter.type,
-            completionAnyCast,
-        )
+        val completionAnyCast = buildRefCastToAny(builder, builder.irGet(structRefType, completionParameter.symbol))
         val coroutineResultType = function.typeParameters[arity].defaultType
         val resumeSymbol = completionParameter.type.classOrFail.getSimpleFunction("resumeWith")
             ?: throw RuntimeException("Could not find function Continuation.resumeWith")
-        val resultClass = context.irBuiltIns.findClass(Name.identifier("Result"))
-            ?: throw RuntimeException("Could not find Result class")
-        val resultConstructor = resultClass.owner.primaryConstructor
-            ?: throw RuntimeException("Result class has no primary constructor")
-        val resultConstructorCall = builder.irCall(resultConstructor.symbol, resultClass.typeWith(coroutineResultType)).apply {
-            putValueArgument(0, invokeCall)
-        }
+        val catchVariable = buildVariable(
+            function,
+            function.startOffset,
+            function.endOffset,
+            IrDeclarationOrigin.CATCH_PARAMETER,
+            Name.identifier("t"),
+            context.irBuiltIns.throwableType,
+        )
+        val tryExpression = builder.irTry(
+            resultClass.typeWith(coroutineResultType),
+            builder.irCall(resultSuccess).apply {
+                putTypeArgument(0, coroutineResultType)
+                dispatchReceiver = builder.irGetObject(resultCompanion.symbol)
+                putValueArgument(0, invokeCall)
+            },
+            listOf(builder.irCatch(catchVariable, builder.irCall(resultFailure).apply {
+                putTypeArgument(0, coroutineResultType)
+                dispatchReceiver = builder.irGetObject(resultCompanion.symbol)
+                putValueArgument(0, builder.irGet(catchVariable))
+            })),
+            null,
+        )
         val resumeCall = builder.irCall(resumeSymbol).apply {
-            dispatchReceiver = completionInterfaceCast
-            putValueArgument(0, resultConstructorCall)
+            dispatchReceiver = completionAnyCast
+            putValueArgument(0, tryExpression)
         }
         return IrExpressionBodyImpl(resumeCall)
     }
-}
 
-/*private fun DeclarationIrBuilder.buildTypeCast(function: IrSimpleFunction, toType: IrType, expression: IrExpression): IrTypeOperatorCall {
-    return IrTypeOperatorCallImpl(
-        function.startOffset,
-        function.endOffset,
-        toType,
-        IrTypeOperator.CAST,
-        toType,
-        expression,
-    )
-}*/
+    private fun buildRefCastToAny(builder: DeclarationIrBuilder, expression: IrExpression): IrCall =
+        builder.irCall(context.wasmSymbols.refCastNull, type = anyType).apply {
+            putTypeArgument(0, anyType)
+            putValueArgument(0, expression)
+        }
+}
